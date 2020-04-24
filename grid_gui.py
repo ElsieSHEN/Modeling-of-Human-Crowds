6f2df6df49cf1b39cf19726abab18259d444a9c6
123Kartoffelbrei <jan.watter@gmail.com>
2020-04-24 14:13:44 +0200

    minor changes

--- grid_gui.py
+++ grid_gui.py
@@ -10,9 +10,10 @@ from tkinter import *
 import numpy as np
 
 master = Tk();
-grid = [5 ,5]
-Width = 50
-(x, y) = (5,5)
+n = 5                       #gridsize
+mat = np.zeros((n,n))       #distance matrix
+Width = 50                  #pixelwidth for GUI
+(x, y) = (n,n)              #numner of cells for GUI
 board = Canvas(master, width=x*Width, height =y*Width)
 board.pack(side=LEFT)
 
@@ -20,8 +21,6 @@ for i in range(x):
         for j in range(y):
             board.create_rectangle(i*Width, j*Width, (i+1)*Width, (j+1)*Width, fill="white", width=1)
             
-grid = [[0 for row in range(x)] for col in range(y)]
-
 var = StringVar(master)
 var.set("Select item")
 
@@ -32,13 +31,13 @@ def set_inital_conditions(event):
     x, y = int(event.x/50), int(event.y/50)
     print("x,y = ", x,y)
     if var.get() == "pedestrian":
-        grid[x][y] = 1
+        mat[x][y] = 1
         board.create_rectangle(x*Width, y*Width, (x+1)*Width, (y+1)*Width, fill = "green", width = 1)
     elif var.get() == "obstacle":
-        grid[x][y] = 2
+        mat[x][y] = 2
         board.create_rectangle(x*Width, y*Width, (x+1)*Width, (y+1)*Width, fill = "blue", width = 1)
     elif var.get() == "target":
-        grid[x][y] = 10
+        mat[x][y] = 10
         board.create_rectangle(x*Width, y*Width, (x+1)*Width, (y+1)*Width, fill = "red", width = 1)    
         
 board.bind('<Button-1>', set_inital_conditions)
